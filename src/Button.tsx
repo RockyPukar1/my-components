5f792bc027faa7734006957e11eef48f10362f7a
Rocky Pukar <ckrp2018@gmail.com>
2023-04-24 16:31:57 +0545

    before accessability addon

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,20 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  /**
+   * Provide a text for the button
+   */
   children: ReactNode;
+
+  /**
+   * Which variant look would you like to use
+   */
   variant: 'primary' | 'secondary';
 }
 
+/**
+ * This is a special button!!!
+ */
 export const Button = ({ children, variant = 'primary', ...props }: Props) => {
   return (
     <button
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Button, Props } from '../src/Button';
-
+import {action} from '@storybook/addon-actions';
 
 const meta: Meta = {
     title: 'Button',
     component: Button,
+    argTypes: {
+        onClick: {action: 'clicked'}
+    }
 }
 
 export default meta;
@@ -20,5 +23,6 @@ Default.args = {
 export const Secondary = Template.bind({});
 Secondary.args = {
     variant: 'secondary',
-    children: 'I am secondary'
+    children: 'I am secondary',
+    onClick: action('secondary click')
 }
