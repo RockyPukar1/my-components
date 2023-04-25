942ead8801bd4efbe37237dbabeef4880219ac75
Rocky Pukar <ckrp2018@gmail.com>
2023-04-25 10:07:50 +0545

    Fine

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { Button, Props } from '../src/Button';
-import {action} from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 
-const meta: Meta = {
-    title: 'Button',
-    component: Button,
-    argTypes: {
-        onClick: {action: 'clicked'}
-    }
-}
+const meta: Meta<typeof Button> = {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
+};
 
 export default meta;
 
+type Story = StoryObj<typeof Button>;
+// const Template: Story = (args) => <Button {...args} />
 
-const Template: Story<Props> = (args) => <Button {...args} />
+export const Default: Story = {
+  args: {
+    children: 'DEFAULT TEXT',
+  },
+};
 
-export const Default = Template.bind({});
-Default.args = {
-    children: 'DEFAULT TEXT'
-}
-export const Secondary = Template.bind({});
-Secondary.args = {
+
+export const Secondary: Story = {
+  args: {
     variant: 'secondary',
     children: 'I am secondary',
-    onClick: action('secondary click')
-}
+    onClick: action('secondary click'),
+  },
+};
